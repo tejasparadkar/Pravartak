4d1de33ba1456f39a959ecab23d424505be69ebb
OmkarSsawant <omkar18sawant@gmail.com>
2024-04-20 04:08:04 +0530

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,8 @@ function App() {
   })
 
 
+  
+
  
 
   async function createPkg(
@@ -148,7 +150,7 @@ function getTendersOfSupplier(supplier:Address){
   return readContract(config,{
     abi,
     address:"0x5FbDB2315678afecb367f032d93F642f64180aa3",
-    functionName:"getTenderDataOfSupplier",
+    functionName:"getTendersOfSupplier",
     args:[supplier]        
   })
 }
@@ -167,7 +169,7 @@ function getTendersOfReceiver(receiver:Address){
  return  readContract(config,{
     abi,
     address:"0x5FbDB2315678afecb367f032d93F642f64180aa3",
-    functionName:"getTendersOfCutomer",
+    functionName:"getTendersOfReceiver",
     args:[
       receiver
     ]        
@@ -224,7 +226,8 @@ function getTendersOfReceiver(receiver:Address){
 
 console.log(
   await createPkg(
-    "0x70997970C51812dc3A010C7d01b50e0d17dc79C8","0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+    "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+    "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
   "NY",
   "AMS",
   "0.1",
@@ -244,6 +247,26 @@ console.log(
 
           
         }}>upload Pkg</button>
+
+
+<button onClick={async ev => {
+    const tenders = await getTendersOfSupplier("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
+    console.log(tenders);
+    
+}} >Get Supplier Tenders</button>
+
+<button onClick={async ev => {
+    const tenders = await getTendersOfCustomer("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+    console.log(tenders);
+    
+}}>Get Customer Tenders</button>
+
+<button onClick={async ev => {
+    const tenders = await getTendersOfReceiver("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
+    console.log(tenders);
+    
+}}>Get Receiver Tenders</button>
+
       </div>
     </>
   )
